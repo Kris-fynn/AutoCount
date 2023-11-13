aea166c72466cb9b7ac2f7037b7594764d7094e8
sphiw <sphiw@LAPTOP-E0Q2LFTM>
2023-11-13 08:09:55 +0200

    Add project files.

---
+++ AutoCount/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace AutoCount
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblScreen = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.AutoCount = new System.Windows.Forms.Timer(this.components);
+            this.psbutton = new System.Windows.Forms.Button();
+            this.rstbutton = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.opnbutton1 = new System.Windows.Forms.Button();
+            this.clsbutton = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblScreen
+            // 
+            this.lblScreen.AutoSize = true;
+            this.lblScreen.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScreen.Location = new System.Drawing.Point(161, 133);
+            this.lblScreen.Name = "lblScreen";
+            this.lblScreen.Size = new System.Drawing.Size(258, 135);
+            this.lblScreen.TabIndex = 0;
+            this.lblScreen.Text = "000";
+            this.lblScreen.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblScreen.Click += new System.EventHandler(this.lblScreen_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Stencil", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(106, 326);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(110, 62);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // AutoCount
+            // 
+            this.AutoCount.Interval = 1000;
+            this.AutoCount.Tick += new System.EventHandler(this.AutoCount_Tick);
+            // 
+            // psbutton
+            // 
+            this.psbutton.Font = new System.Drawing.Font("Stencil", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.psbutton.Location = new System.Drawing.Point(244, 326);
+            this.psbutton.Name = "psbutton";
+            this.psbutton.Size = new System.Drawing.Size(110, 62);
+            this.psbutton.TabIndex = 2;
+            this.psbutton.Text = "Pause";
+            this.psbutton.UseVisualStyleBackColor = true;
+            this.psbutton.Click += new System.EventHandler(this.psbutton_Click);
+            // 
+            // rstbutton
+            // 
+            this.rstbutton.Font = new System.Drawing.Font("Stencil", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rstbutton.Location = new System.Drawing.Point(389, 326);
+            this.rstbutton.Name = "rstbutton";
+            this.rstbutton.Size = new System.Drawing.Size(110, 62);
+            this.rstbutton.TabIndex = 3;
+            this.rstbutton.Text = "Reset";
+            this.rstbutton.UseVisualStyleBackColor = true;
+            this.rstbutton.Click += new System.EventHandler(this.rstbutton_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(632, 56);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.DropDown += new System.EventHandler(this.comboBox1_DropDown);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "1200",
+            "9600",
+            "115200"});
+            this.comboBox2.Location = new System.Drawing.Point(632, 111);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 24);
+            this.comboBox2.TabIndex = 5;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(633, 179);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 23);
+            this.numericUpDown1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Stencil", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(537, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 16);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Com Port";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Stencil", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(537, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 16);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Baud Rate";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Stencil", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(537, 181);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 16);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Address";
+            // 
+            // opnbutton1
+            // 
+            this.opnbutton1.Font = new System.Drawing.Font("Stencil", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.opnbutton1.Location = new System.Drawing.Point(657, 245);
+            this.opnbutton1.Name = "opnbutton1";
+            this.opnbutton1.Size = new System.Drawing.Size(75, 34);
+            this.opnbutton1.TabIndex = 11;
+            this.opnbutton1.Text = "Open";
+            this.opnbutton1.UseVisualStyleBackColor = true;
+            this.opnbutton1.Click += new System.EventHandler(this.opnbutton1_Click);
+            // 
+            // clsbutton
+            // 
+            this.clsbutton.Font = new System.Drawing.Font("Stencil", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clsbutton.Location = new System.Drawing.Point(657, 301);
+            this.clsbutton.Name = "clsbutton";
+            this.clsbutton.Size = new System.Drawing.Size(75, 31);
+            this.clsbutton.TabIndex = 12;
+            this.clsbutton.Text = "Close";
+            this.clsbutton.UseVisualStyleBackColor = true;
+            this.clsbutton.Click += new System.EventHandler(this.clsbutton_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(184, 56);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(192, 23);
+            this.progressBar1.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Snow;
+            this.ClientSize = new System.Drawing.Size(914, 450);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.clsbutton);
+            this.Controls.Add(this.opnbutton1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.rstbutton);
+            this.Controls.Add(this.psbutton);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblScreen);
+            this.Font = new System.Drawing.Font("Stencil", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "AutoCountReset";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblScreen;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Timer AutoCount;
+        private System.Windows.Forms.Button psbutton;
+        private System.Windows.Forms.Button rstbutton;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button opnbutton1;
+        private System.Windows.Forms.Button clsbutton;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ AutoCount/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Header;
+using ComboBox = System.Windows.Forms.ComboBox;
+
+namespace AutoCount
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            getAllPort();
+        }
+
+        int timeRight = 0;
+
+        void getAllPort()
+        {
+            string[] s = SerialPort.GetPortNames();
+            comboBox1.Items.Clear();
+            comboBox1.Items.AddRange(s);
+        }
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            timeRight = 0;
+            AutoCount.Start();
+        }
+
+        private void lblScreen_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AutoCount_Tick(object sender, EventArgs e)
+        {
+            if (timeRight < 100)
+            {
+                timeRight = timeRight++;
+                lblScreen.Text = timeRight + "";
+                var TimeSpanVar = TimeSpan.FromSeconds(timeRight);//take timeleft from seconds to hours and put it timespanvar variable
+                lblScreen.Text = TimeSpanVar.ToString();//display the value on the timespan variable on label1
+            }
+
+            if (lblScreen.Text == "100")
+            {
+                timeRight = 0;
+            }
+
+            lblScreen.Text = timeRight++.ToString();
+            List<byte> PacketToSend = new List<byte>();//craete a list/packet to send to serialport
+            PacketToSend.Add(0);//sign address that the communication is sent to 
+            PacketToSend.Add((byte)'S');//sign address that the communication is sent to 
+            PacketToSend.Add(1);//sign address that the communication is sent to 
+            PacketToSend.Add(3);//end of header
+
+            string val1 = lblScreen.Text;//take the information displayed on label1 and put in the string val1
+            for (int i = 0; i < val1.Length; i++)
+            {
+                PacketToSend.Add((byte)(Char)val1[i]);//add the value on val1 to the list/packet that will be sent to the display
+            }
+
+            PacketToSend.Add(4);//end of text indiscation to the  display
+
+            byte crc = 0;
+            for (int i = 0; i < PacketToSend.Count; i++)
+            {
+                crc ^= PacketToSend[i];
+            }
+            PacketToSend.Add(crc);
+            String StringToSend = "";
+            for (int i = 0; i < PacketToSend.Count; i++)
+            {
+                StringToSend += (Char)PacketToSend[i];
+            }
+            serialPort1.Write(StringToSend);
+
+        }
+        private void psbutton_Click(object sender, EventArgs e)
+        {
+            timeRight = 100;
+            AutoCount.Stop();
+        }
+
+        private void rstbutton_Click(object sender, EventArgs e)
+        {
+            AutoCount.Stop();
+            timeRight = 100;
+            lblScreen.Text = "000";
+        }
+
+        private void opnbutton1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.PortName = comboBox1.Text;
+                serialPort1.BaudRate = Convert.ToInt32((comboBox2.Text));
+                serialPort1.Open();
+                progressBar1.Value = 100;
+            }
+            catch (Exception)
+            {
+                //Do nothing
+            }
+
+
+        }
+
+        private void clsbutton_Click(object sender, EventArgs e)
+        {
+            serialPort1.Close();
+            progressBar1.Value = 0;
+        }
+
+        private void comboBox1_DropDown(object sender, EventArgs e)
+        {
+            getAllPort();
+        }
+    }
+    }
+
